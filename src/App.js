c924022537f668b65ac533fb5c2f40287fb09a83
TaherMAfini <tma29@njit.edu>
2024-05-04 15:20:32 -0400

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import * as d3 from "d3";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      <div className="container-fluid text-center">
+        <div className="row">
+          <div className="col">
+            <h1>Column 1</h1>
+          </div>
+          <div className="col">
+            <h1>Column 2</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
